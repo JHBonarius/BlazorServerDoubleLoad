a99a4ade4c35c1ab95a496501c9c3ffa2958bc27
Jochem Bonarius <jochem@netwinst.nl>
2021-03-21 08:40:29 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServerDoubleLoad.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Home/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
